00f2b571ed6f411099673e2fe4159cf008adfefe
Shubhamjan <shubhamjangale626@gmail.com>
2025-11-23 23:45:58 +0530

    ref: operators learning going on

---
+++ Operators.py
@@ -0,0 +1,37 @@
+a=11
+b=2
+# print(a//b) 
+#floor divison
+
+# print(a%b)
+#Modulus
+
+# print(a**b)
+#Exponentiation
+
+#Identity Operators(is or is not)
+
+a = 23
+b = 34
+c = a
+# print(a is not b)
+# print(c is a) #a--->23<----b
+
+
+# Membership operator(in and not in)
+x = 20
+y = 50
+list = [3,5,23,30,20]
+if(x in list):
+    print( "x is in list" )
+else:
+    print("x is not in list" )
+
+if(x not in list):
+    print( "x is in list" )
+else:
+    print("x is not in list" )
+
+#Ternary operator
+print(x if x>y else y)
+
