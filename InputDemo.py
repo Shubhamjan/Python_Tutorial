cd2c9b8c095feb38b7fa781b3b2b32b07406cd4b
Shubhamjan <shubhamjangale626@gmail.com>
2025-11-23 19:27:51 +0530

    ref:added the input

---
+++ InputDemo.py
@@ -0,0 +1,22 @@
+# name = input("Enter your name:")
+# age = int(input("Enter the:"))
+# print(name,age)
+
+#Taking the multiple values
+# x,y = input("Enter two values :").split()
+# print("Number of boys :"+x)
+# print("Number of girls :"+y)
+
+#Take the number
+# n = int(input("Enter the number :"))
+# print(n)
+
+#Take the float
+# n = float(input("Enter the price...."))
+# print(n)
+
+#Check the type
+print(type(5))
+print(type(("Shubham","Om")))
+print(type(["shubham",2]))
+print(type({"name":"shubham","age":23}))
