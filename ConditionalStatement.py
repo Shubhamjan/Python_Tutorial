2e6042ee649d74949b5c128ae74df7066d9e1bbc
Shubhamjan <shubhamjangale626@gmail.com>
2025-11-27 07:42:26 +0530

    ref: added the conditionalstatement

---
+++ ConditionalStatement.py
@@ -0,0 +1,20 @@
+# if 10>12:
+#     print("bad")
+# else:
+#     print("true")
+# #----------------------------
+# if 10>12:
+#     print("true")
+# elif 20<18:
+#     print("false")
+# else:
+#     print("yes")
+
+# Match case statement
+n = 5
+
+match n:
+    case 1:
+        print("shubham")
+    case 5:
+        print("Engineer")
